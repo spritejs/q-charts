70ff7bb28f7d49fcd1163fbd270a27d9aa7303ad
摇太阳 <yaotaiyang99@gmail.com>
2019-06-17 15:53:26 +0800

    update docs

--- demo/components/BlockDemo.vue
+++ demo/components/BlockDemo.vue
@@ -213,7 +213,9 @@ export default {
   justify-content: flex-end;
   align-items: center;
 }
-
+.block-demo--fixed .bock-demo__ctrl {
+  padding-right: 88px;
+}
 .bock-demo__ctrl button + button {
   margin-left: 10px;
 }
