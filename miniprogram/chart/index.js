cb487c4044bac328b9dc1c30bdcc0ecf90297043
akira-cn <akira.cn@gmail.com>
2019-06-27 18:27:14 +0800

    增加小程序组件

---
+++ miniprogram/chart/index.js
@@ -0,0 +1,61 @@
+const spritejs = require('@spritejs/wxapp'); // eslint-disable-line no-unused-vars
+const { Chart } = require('../index');
+
+/* globals Component: true */
+Component({
+  properties: {
+    chartId: {
+      type: String,
+      value: 'q-chart',
+    },
+    width: {
+      type: Number,
+      value: 750,
+    },
+    height: {
+      type: Number,
+      value: wx.getSystemInfoSync().windowHeight,
+    },
+    eventOffset: {
+      type: Array,
+      value: null,
+    },
+  },
+  methods: {
+    updateEventOffset() {
+      const query = wx.createSelectorQuery().in(this);
+      query.select('.scene-layout').boundingClientRect().exec(([rect]) => {
+        if (rect) {
+          this.setData({
+            eventOffset: [rect.left, rect.top],
+          });
+        }
+      });
+    },
+    onTouchStart(event) {
+      this.chart.scene.delegateEvent(event, this.data.eventOffset);
+    },
+    onTouchMove(event) {
+      this.chart.scene.delegateEvent(event, this.data.eventOffset);
+    },
+    onTouchEnd(event) {
+      this.chart.scene.delegateEvent(event, this.data.eventOffset);
+    },
+    onTap(event) {
+      this.chart.scene.delegateEvent(event, this.data.eventOffset);
+    },
+    onLongPress(event) {
+      this.chart.scene.delegateEvent(event, this.data.eventOffset);
+    },
+  },
+  ready() {
+    if (!this.data.eventOffset) this.updateEventOffset();
+    const chart = new Chart({
+      container: '#app',
+      layer: this.data.chartId,
+      size: [this.data.width, this.data.height],
+    });
+    this.triggerEvent('ChartCreated', { chart });
+    this.chart = chart;
+  },
+});
