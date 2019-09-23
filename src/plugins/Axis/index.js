ce83c1926a0a784511f81582f46e05c1408f4e65
yaotaiyang <yaotaiyang99@gmail.com>
2019-09-23 16:13:57 +0800

    bug fixed for axis label formatter

--- src/plugins/Axis/index.js
+++ src/plugins/Axis/index.js
@@ -24,53 +24,60 @@ export class Axis extends BasePlugin {
       range: undefined, // 刻度范围
       coord: 'cartesian2d', // [cartesian2d,polar]
       coordPos: ['50%', '50%'],
-      formatter: function(str, data) { // 格式化坐标轴文字显示
+      formatter: function(str, data) {
+        // 格式化坐标轴文字显示
         return str
       }
     }
-    return dObj;
+    return dObj
   }
 
   beforeRender() {
     super.beforeRender()
     const data = this.getData()
-    this.prepareAttrs(this.chart.dataset.attr());
-    this.renderData = layout({ ...this.attr(), data });
+    this.prepareAttrs(this.chart.dataset.attr())
+    this.renderData = layout({ ...this.attr(), data })
     this.renderData.scales.forEach(scale => {
       scale.from = scale.to = { pos: scale.pos }
-      scale.labelFrom = scale.labelTo = { text: getNumberText(scale.text, this.attr()) }
+      scale.labelFrom = scale.labelTo = {
+        text: getNumberText(scale.text, this.attr())
+      }
     })
-    return this.renderData;
+    return this.renderData
   }
 
   beforeUpdate() {
     super.beforeRender()
     const data = this.getData()
-    this.prepareAttrs(this.chart.dataset.attr());
-    let oldRenderData = this.renderData;
+    this.prepareAttrs(this.chart.dataset.attr())
+    let oldRenderData = this.renderData
     this.renderData = layout({ ...this.attr(), data })
     this.renderData.scales.forEach((scale, i) => {
-      let from = { pos: scale.pos };
+      let from = { pos: scale.pos }
       let labelFrom = { text: getNumberText(scale.text, this.attr()) }
       if (oldRenderData.scales[i]) {
         from = { pos: oldRenderData.scales[i].pos }
-        labelFrom = { text: getNumberText(oldRenderData.scales[i].text, this.attr()) }
+        labelFrom = {
+          text: getNumberText(oldRenderData.scales[i].text, this.attr())
+        }
       }
       scale.from = from
       scale.to = { pos: scale.pos }
-      scale.labelFrom = labelFrom;
+      scale.labelFrom = labelFrom
       scale.labelTo = { text: getNumberText(scale.text, this.attr()) }
     })
-    return this.renderData;
+    return this.renderData
   }
-  prepareAttrs(fields) { // 处理默认属性，部分属性会默认从对应visula继承过来
+  prepareAttrs(fields) {
+    // 处理默认属性，部分属性会默认从对应visula继承过来
     let { target: $target, orient, axisGap } = this.attr()
-    let optionAttrs = this.optionAttrs;
+    let optionAttrs = this.optionAttrs
     let type = 'category' // 默认按照category分类进行
-    let field = fields.text;
-    if (this.chart.visuals.length >= 1 && $target === undefined) { // 如果visual大于等于一个
+    let field = fields.text
+    if (this.chart.visuals.length >= 1 && $target === undefined) {
+      // 如果visual大于等于一个
       $target = this.chart.visuals[0]
-      this.attr({ target: $target });
+      this.attr({ target: $target })
     }
     if ($target && $target.attr('transpose')) {
       if (orient === 'top' || orient === 'bottom') {
@@ -82,16 +89,16 @@ export class Axis extends BasePlugin {
 
     this.attr({ type, field })
 
-    this.mergeAttr($target, ['stack', 'axisGap', 'size', 'pos']); // 合并来自对应visual的属性
+    this.mergeAttr($target, ['stack', 'axisGap', 'size', 'pos']) // 合并来自对应visual的属性
 
-
-    if (type === 'value') { // 如果为value类型，axisGap强制为false
+    if (type === 'value') {
+      // 如果为value类型，axisGap强制为false
       this.attr({ axisGap: false, field: fields.value })
     }
     if ($target instanceof Scatter) {
       this.attr({ type: 'value' })
       if (orient === 'bottom' || orient === 'top') {
-        this.attr({ field: fields.text });
+        this.attr({ field: fields.text })
       }
     }
 
@@ -100,9 +107,9 @@ export class Axis extends BasePlugin {
     }
 
     if ($target && $target.attr('layoutWay')) {
-      let layoutWay = $target.attr('layoutWay');
-      let field = this.attr('field');
-      let curLayout = layoutWay[field];
+      let layoutWay = $target.attr('layoutWay')
+      let field = this.attr('field')
+      let curLayout = layoutWay[field]
       this.attr({ range: [curLayout.min, curLayout.max] })
     }
 
@@ -110,7 +117,12 @@ export class Axis extends BasePlugin {
       this.attr({ type: optionAttrs.type })
     }
 
-    if (axisGap === undefined && $target instanceof Bar && type === 'category') { // 特殊情况Bar特殊处理
+    if (
+      axisGap === undefined &&
+      $target instanceof Bar &&
+      type === 'category'
+    ) {
+      // 特殊情况Bar特殊处理
       this.attr({ axisGap: true })
     }
   }
@@ -119,7 +131,10 @@ export class Axis extends BasePlugin {
       arr.forEach(name => {
         let tarVal = $target.attr(name)
         let myVal = this.attr(name)
-        if ((this.optionAttrs[name] === undefined || myVal === undefined) && tarVal !== undefined) {
+        if (
+          (this.optionAttrs[name] === undefined || myVal === undefined) &&
+          tarVal !== undefined
+        ) {
           if (name === 'size' || name === 'pos') {
             this.attr('__' + name + '__', tarVal)
           }
@@ -129,145 +144,201 @@ export class Axis extends BasePlugin {
     }
   }
   mergeTheme(name, args) {
-    let themes = this.chart.resolveTheme('Axis')[this.attr('orient')];
+    let themes = this.chart.resolveTheme('Axis')[this.attr('orient')]
     return mergeStyle(this.style(name), args, themes[name])
   }
   render(renderData = {}) {
-    let axisStyle = this.mergeTheme('axis', [renderData.axisAttrs]);
-    let rings = [];
-    const { axisGap, formatter, pos, coord, size, coordPos } = this.attr();
-    let $target = this.attr('target');
+    let axisStyle = this.mergeTheme('axis', [renderData.axisAttrs])
+    let rings = []
+    const { axisGap, formatter, pos, coord, size, coordPos } = this.attr()
+    let $target = this.attr('target')
     if ($target instanceof Pie && coord === 'polar') {
-      const { startAngle, endAngle, padAngle } = $target.attr();
+      const { startAngle, endAngle, padAngle } = $target.attr()
       rings = pieLayout()
         .startAngle(startAngle)
         .endAngle(endAngle)
         .padAngle(padAngle)
-        .value(d => +d[0].__valueGetter__())(this.getData().filter(d => !d[0].disabled));
+        .value(d => +d[0].__valueGetter__())(
+          this.getData().filter(d => !d[0].disabled)
+        )
     }
     return (
       <Group pos={pos}>
         <Group clipOverflow={false}>
           {renderData.scales.map((scale, i) => {
-            let labelStyle = this.mergeTheme('label', [renderData.labelAttrs, scale, i]);
-            let scaleStyle = this.mergeTheme('scale', [renderData.scaleAttrs, scale, i]);
-            let gridStyle = this.mergeTheme('grid', [renderData.gridAttrs, scale, i]);
+            let labelStyle = this.mergeTheme('label', [
+              renderData.labelAttrs,
+              scale,
+              i
+            ])
+            let scaleStyle = this.mergeTheme('scale', [
+              renderData.scaleAttrs,
+              scale,
+              i
+            ])
+            let gridStyle = this.mergeTheme('grid', [
+              renderData.gridAttrs,
+              scale,
+              i
+            ])
             let labelAnimation = {
               from: scale.labelFrom,
               to: scale.labelTo,
               duration: 200,
-              attrFormatter: (attr) => {
+              attrFormatter: attr => {
                 if (typeof attr.text === 'number') {
-                  return Object.assign(attr, { text: formatter(formatNumber(attr.text, scale.labelFrom.text, scale.labelTo.text)) });
+                  let text = formatter(
+                    formatNumber(
+                      attr.text,
+                      scale.labelFrom.text,
+                      scale.labelTo.text
+                    )
+                  )
+                  let resAttr = Object.assign(attr, {
+                    text: text
+                  })
+                  return resAttr
                 }
                 return attr
               },
               useTween: true
             }
-            if (typeof scale.labelTo.text !== 'number' || typeof scale.labelFrom.text !== 'number') {
+            if (
+              typeof scale.labelTo.text !== 'number' ||
+              typeof scale.labelFrom.text !== 'number'
+            ) {
               labelAnimation = {}
             }
             return (
-              <Group size={[1, 1]} pos={scale.from.pos} clipOverflow={false}
+              <Group
+                size={[1, 1]}
+                pos={scale.from.pos}
+                clipOverflow={false}
                 animation={this.resolveAnimation({
                   from: scale.from,
                   to: scale.to,
                   duration: 200,
                   useTween: true
-                })}>
-                {
-                  labelStyle === false ? null : (
-                    <Label {...labelStyle} clipOverflow={false} text={scale.labelTo.text} animation={this.resolveAnimation(labelAnimation)}></Label>
-                  )
-                }
-                {
-                  scaleStyle === false ? null : (
-                    <Rect {...scaleStyle}/>
-                  )
-                }
-                {
-                  (coord === 'polar' || gridStyle === false || (scale.offset === 0 && !axisGap)) ? null : (
-                    <Polyline {...gridStyle}/>
-                  )
-                }
+                })}
+              >
+                {labelStyle === false ? null : (
+                  <Label
+                    {...labelStyle}
+                    clipOverflow={false}
+                    text={formatter(scale.labelTo.text)}
+                    animation={this.resolveAnimation(labelAnimation)}
+                  ></Label>
+                )}
+                {scaleStyle === false ? null : <Rect {...scaleStyle} />}
+                {coord === 'polar' ||
+                gridStyle === false ||
+                (scale.offset === 0 && !axisGap) ? null : (
+                    <Polyline {...gridStyle} />
+                  )}
               </Group>
             )
           })}
         </Group>
         <Group size={size}>
           {renderData.scales.map((scale, i) => {
-            let gridStyle = this.mergeTheme('grid', [renderData.gridAttrs, scale, i]);
-            return (
-              (coord !== 'polar' || gridStyle === false || (scale.offset === 0 && !axisGap)) ? null : (
-                <Circle pos={coordPos} radius={scale.offset} {...gridStyle} anchor={[0.5]}/>
+            let gridStyle = this.mergeTheme('grid', [
+              renderData.gridAttrs,
+              scale,
+              i
+            ])
+            return coord !== 'polar' ||
+              gridStyle === false ||
+              (scale.offset === 0 && !axisGap) ? null : (
+                <Circle
+                  pos={coordPos}
+                  radius={scale.offset}
+                  {...gridStyle}
+                  anchor={[0.5]}
+                />
               )
-            )
           })}
         </Group>
         <Group clipOverflow={false}>
-          {
-            rings.map(ring => { // 绘制射线
-              let angle = (ring.startAngle + ring.endAngle) / 2;
-              let maxRadius = this.attr('target').maxOuterRadius;
-              let txt = ring['0'].__textGetter__()
-              let anchorX = -Math.cos(angle);
-              let anchorY = -Math.sin(angle);
-              let anchor = [0.5, 0.5]
-              if (Math.abs(anchorX) > Math.abs(anchorY)) {
-                anchor[0] = anchorX < 0 ? 0 : anchorX
-              } else {
-                anchor[1] = anchorY > 0 ? anchorY : 0
-              }
-              let ang = Math.abs(angle / Math.PI * 180 % 90); // 角度转换为[0-90];
-              let k = Math.abs(45 - Math.abs(ang - 45)) / 45; // 相关数据转化为[0-1]
-              let labelDis = maxRadius * (1.01 + k / 18) // 18为影响因子 1.01为基准距离
-              let pos = transPx(coordPos, size);
-              let newPoint = [pos[0] + maxRadius * Math.cos(angle), pos[1] + maxRadius * Math.sin(angle)]
-              let labelPoint = [pos[0] + labelDis * Math.cos(angle), pos[1] + labelDis * Math.sin(angle)]
-              return (
-                <Group clipOverflow={false} size={['100%', '100%']}>
-                  <Polyline points={[pos, newPoint]} color={'#dde0e5'} lineDash={[3, 4]}/>
-                  <Label pos={labelPoint} color={'#67728C'} text={txt} fontSize={12} anchor={anchor}/>
-                </Group>
-              )
-            })
-          }
+          {rings.map(ring => {
+            // 绘制射线
+            let angle = (ring.startAngle + ring.endAngle) / 2
+            let maxRadius = this.attr('target').maxOuterRadius
+            let txt = ring['0'].__textGetter__()
+            let anchorX = -Math.cos(angle)
+            let anchorY = -Math.sin(angle)
+            let anchor = [0.5, 0.5]
+            if (Math.abs(anchorX) > Math.abs(anchorY)) {
+              anchor[0] = anchorX < 0 ? 0 : anchorX
+            } else {
+              anchor[1] = anchorY > 0 ? anchorY : 0
+            }
+            let ang = Math.abs(((angle / Math.PI) * 180) % 90) // 角度转换为[0-90];
+            let k = Math.abs(45 - Math.abs(ang - 45)) / 45 // 相关数据转化为[0-1]
+            let labelDis = maxRadius * (1.01 + k / 18) // 18为影响因子 1.01为基准距离
+            let pos = transPx(coordPos, size)
+            let newPoint = [
+              pos[0] + maxRadius * Math.cos(angle),
+              pos[1] + maxRadius * Math.sin(angle)
+            ]
+            let labelPoint = [
+              pos[0] + labelDis * Math.cos(angle),
+              pos[1] + labelDis * Math.sin(angle)
+            ]
+            return (
+              <Group clipOverflow={false} size={['100%', '100%']}>
+                <Polyline
+                  points={[pos, newPoint]}
+                  color={'#dde0e5'}
+                  lineDash={[3, 4]}
+                />
+                <Label
+                  pos={labelPoint}
+                  color={'#67728C'}
+                  text={formatter(txt)}
+                  fontSize={12}
+                  anchor={anchor}
+                />
+              </Group>
+            )
+          })}
         </Group>
-        {
-          axisStyle === false ? null : (
-            <Rect {...axisStyle} pos={renderData.originalPoint}/>
-          )
-        }
+        {axisStyle === false ? null : (
+          <Rect {...axisStyle} pos={renderData.originalPoint} />
+        )}
       </Group>
     )
   }
 }
 function transPx(point, size) {
   return point.map((num, i) => {
-    let ind = String(num).indexOf('%');
+    let ind = String(num).indexOf('%')
     if (ind !== -1) {
-      return size[i] * num.substr(0, ind) / 100
+      return (size[i] * num.substr(0, ind)) / 100
     }
     return num
   })
 }
 function formatNumber(str, fromStr, toStr) {
-  if (typeof str === 'number' && typeof fromStr === 'number' && typeof toStr === 'number') {
+  if (
+    typeof str === 'number' &&
+    typeof fromStr === 'number' &&
+    typeof toStr === 'number'
+  ) {
     let len = getDigit(toStr)
     return str.toFixed(len)
   }
   return toStr
 }
 function getDigit(num) {
-  let arrNum = String(num).split('.');
+  let arrNum = String(num).split('.')
   if (arrNum.length > 1) {
     return arrNum[1].length
   }
-  return 0;
+  return 0
 }
 function getNumberText(str, attrs) {
   if (!isNaN(str) && attrs.type === 'value') {
     return Number(str)
   }
-  return str;
+  return str
 }
